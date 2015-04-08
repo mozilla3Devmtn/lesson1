76d77e2230e2fdff1152a623b9bda1de98e452ef
Mozilla3 <mozilla3@gmail.com>
2015-04-07 20:41:54 -0600

    another try

--- myName.js
+++ myName.js
@@ -9,7 +9,9 @@ var yourAge = prompt("what is your age?");
 var yourColor = prompt("what's your favorite color?");
 
 for (var i = 0; i < 10, i++) {
-  if myCar === yourCar {
-    alert("awesome " + yourName + ", I drive a " + myCar "too!")
+  if (myCar === yourCar) {
+    alert("awesome " + yourName + ", I drive a " + myCar + "too!");
   }
+  else {
+    alert("nice car" + yourName + "!");
 }
