304cb3111d00db0b669224401568e559f3015c79
mozilla3Devmtn <mozilla3@gmail.com>
2015-04-07 20:29:37 -0600

    Update myName.js

--- myName.js
+++ myName.js
@@ -2,4 +2,14 @@
 var myName = "Matt";
 var myColor = "Blue"; 
 var myAge = 35;
+var myCar = "is300";
+var yourName = prompt("what's your name?")
+var yourCar = prompt("what car do you drive?");
+var yourAge = prompt("what is your age?");
+var yourColor = prompt("what's your favorite color?");
 
+for (var i = 0; i < 10, i++) {
+  if myCar === yourCar {
+    alert("awesome " + yourName + ", I drive a " + myCar "too!")
+  }
+}
